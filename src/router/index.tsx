f4ee494e74db021376517ecb71c33ceb1a240f35
dragunshin <sikk337@naver.com>
2026-01-19 09:48:10 +0000

    feat: feat: 솔루션 페이지 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -36,6 +36,7 @@ import ReviewWritePage from "@/pages/myPage/review/writeReview";
 import MyReviewPage from "@/pages/myPage/review/myReview";
 import ConsultationGate from "@/pages/solution/page";
 import SolutionView from "@/pages/solution/readSolution";
+import ConcernView from "@/pages/solution/customer/readConcern";
 
 export const router = createBrowserRouter([
   {
@@ -228,7 +229,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/consultations/:consultationId/solution",
-    element: <SolutionView />,
+
     element: (
       <AuthGuard>
         <ConcernView />
